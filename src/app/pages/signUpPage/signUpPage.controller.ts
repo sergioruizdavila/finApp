18ec519783a90f271610f98b8703a42ed58e38ff
Sergio Ruiz Davila <sergioruizdavila@gmail.com>
2016-04-01 08:21:04 -0500

    Mod: Refactor the way of translate text in each html

--- src/app/pages/signUpPage/signUpPage.controller.ts
+++ src/app/pages/signUpPage/signUpPage.controller.ts
@@ -51,17 +51,21 @@ module app.pages.signUpPage {
 
         /*-- INJECT DEPENDENCIES --*/
         public static $inject = ['$ionicHistory',
-                                '$state',
-                                '$stateParams',
-                                'finApp.auth.AuthService'];
+            '$ionicPopup',
+            '$state',
+            '$stateParams',
+            'finApp.auth.AuthService',
+            '$filter'];
 
         /**********************************/
         /*           CONSTRUCTOR          */
         /**********************************/
         constructor(private $ionicHistory: ionic.navigation.IonicHistoryService,
-                    private $state: ng.ui.IStateService,
-                    private $stateParams: ISignUpDataConfig,
-                    private AuthService) {
+            private $ionicPopup: ionic.popup.IonicPopupService,
+            private $state: ng.ui.IStateService,
+            private $stateParams: ISignUpDataConfig,
+            private AuthService,
+            private $filter) {
 
             this.init();
 
@@ -119,16 +123,53 @@ module app.pages.signUpPage {
         */
         register(): void {
             let self = this;
+            //CONSTANTS
+            const POPUP_TITLE = this.$filter('translate')('%popup.create_user.title.text');
+            const POPUP_BODY_TEXT = this.$filter('translate')('%popup.create_user.body_message.text');
+            const POPUP_CANCEL_BUTTON_TEXT = this.$filter('translate')('%popup.general.cancel_button.text');
+            const POPUP_OK_BUTTON_TEXT = this.$filter('translate')('%popup.create_user.ok_button.text');
+            const POPUP_OK_BUTTON_TYPE = 'button-positive';
+            /********************/
+
+            let confirmInstance = self.$ionicPopup.show({
+                title: POPUP_TITLE,
+                template: POPUP_BODY_TEXT,
+                buttons: [
+                    { text: POPUP_CANCEL_BUTTON_TEXT },
+                    {
+                        text: POPUP_OK_BUTTON_TEXT,
+                        type: POPUP_OK_BUTTON_TYPE,
+                        onTap: function(e) {
+                            console.log('onTap Button Event: ', e);
+                        }
+                    }
+                ]
+            });
+
+            confirmInstance.then(function(res) {
+                if (res) {
+                    //Si presiona SI, lo deberia llevar a la funcion login()
+                    self.$state.go('page.logIn', { user: self.user });
+                } else {
+                    console.log('You are not sure');
+                }
+            });
 
             this.user.email = this.form.email;
-            this.AuthService().$createUser(this.user).then(function (user){
+            this.AuthService().$createUser(this.user).then(function(user) {
+                //TODO: replantear esto ya que deberia primero preguntar, y despues si crear la cuenta
+                //toca averiguar como hacer que se intente primero loguear, y si ese email no existe en
+                //la base, entonces mostrar el popUp, y preguntar si el usuario quiere crear una nueva cuenta
+                //Posible solucion: Buscar en la base si existe ya un usuario con ese mail (recorrer todo el objeto users,
+                //y buscar si hay un email igual ya registrado) sino, deberia mostrar el popUp y confirmar la creacion del usuario
+
                 //TODO: Mostrar un popUp diciendo: te crearemos una nueva cuenta: Si o No
-                //Si presiona SI, lo deberia llevar a la funcion login()
+
                 console.log('new user: ', user);
-            }, function (error){
+            }, function(error) {
                 //Llevar a la pagina de Logging si ese mail ya esta registrado
                 if (error.code === 'EMAIL_TAKEN') {
-                    self.$state.go('page.logIn', { user: self.user});
+                    self.$state.go('page.logIn', { user: self.user });
                 } else {
                     self.error = error;
                 }
